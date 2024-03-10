b4018f81b4f3d25287481a17191eb9cde16068fc
Philipp Daun <post@philippdaun.net>
2024-03-10 23:03:51 +0100

    Refactor cache node into support class

--- src/Extensions/Nodes/CacheNode.php
+++ src/Extensions/Nodes/CacheNode.php
@@ -32,55 +32,24 @@ final class CacheNode extends StatementNode
     {
         return $context->format(
             <<<'XX'
-				$ʟ_params = %node;
-				$ʟ_main = $node[0] ?? null;
-				$ʟ_if = $ʟ_params['if'] ?? (is_bool($ʟ_main) ? $ʟ_main : true);
-				$ʟ_enabled = $ʟ_if !== false
-					&& config('statamic.system.cache_tags_enabled', true)
-					&& request()->method() === 'GET';
-
-				if ($ʟ_enabled) {
-					$ʟ_key = $ʟ_params['key'] ?? (is_string($ʟ_main) ? $ʟ_main : null);
-					$ʟ_tags = $ʟ_params['tags'] ?? (is_array($ʟ_main) ? $ʟ_main : null);
-					$ʟ_scope = $ʟ_params['scope'] ?? ['site', 'auth'];
-					$ʟ_for = $ʟ_params['for'] ?? (is_integer($ʟ_main) ? $ʟ_main : null);
-					$ʟ_expires = $ʟ_for ? now()->add("+{$ʟ_for}") : null;
-
-					$ʟ_store = \Illuminate\Support\Facades\Cache::store();
-					if (is_array($ʟ_tags) && count($ʟ_tags)) {
-						$ʟ_store = $ʟ_store->tags($ʟ_tags);
-					}
-
-					$auth = auth(config('statamic.users.guards.cp', 'web'));
-
-					$ʟ_hash = [
-						'content' => $ʟ_key ?: %dump,
-						'params' => $ʟ_params,
-						'auth' => $auth->check(),
-						'site' => \Statamic\Facades\Site::current()->handle(),
-						'scope' => collect($ʟ_scope)->flip()->map(function ($_, $scope) {
-							return match($scope) {
-								'page' => \Statamic\Facades\URL::makeAbsolute(\Statamic\Facades\URL::getCurrent()),
-								'user' => ($user = $auth->user()) ? $user->id : 'guest',
-								default => null,
-							};
-						})->all()
-					];
-					$ʟ_key = 'latte.statamic.cache.'.md5(json_encode($ʟ_hash));
-
-					if ($ʟ_output = $ʟ_store->get($ʟ_key)) %line {
-						echo $ʟ_output;
-					} else {
-						ob_start(fn() => '');
-						%node
-						$ʟ_output = ob_get_clean();
-						$ʟ_store->put($ʟ_key, $ʟ_output, $ʟ_expires);
-						echo $ʟ_output;
-					}
-				} else {
-					%node
-				}
-				XX,
+                $ʟ_params = %node;
+                if (\Daun\StatamicLatte\Support\CacheNode::enabled($ʟ_params)) {
+                    $ʟ_store = \Daun\StatamicLatte\Support\CacheNode::store($ʟ_params);
+                    $ʟ_key = \Daun\StatamicLatte\Support\CacheNode::key($ʟ_params, %dump);
+                    $ʟ_expires = \Daun\StatamicLatte\Support\CacheNode::expires($ʟ_params);
+                    if ($ʟ_output = $ʟ_store->get($ʟ_key)) %line {
+                        echo $ʟ_output;
+                    } else {
+                        ob_start(fn() => '');
+                        %node
+                        $ʟ_output = ob_get_clean();
+                        $ʟ_store->put($ʟ_key, $ʟ_output, $ʟ_expires);
+                        echo $ʟ_output;
+                    }
+                } else {
+                    %node
+                }
+                XX,
             $this->args,
             md5($this->content->print($context)),
             $this->position,
