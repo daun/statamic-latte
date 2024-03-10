079a7d74d4327bb92c2eb51efa657b56b7d2bf94
Philipp Daun <post@philippdaun.net>
2024-03-10 23:20:23 +0100

    Test cache duration

--- tests/Feature/CacheTest.php
+++ tests/Feature/CacheTest.php
@@ -1,10 +1,9 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 test('caches contents of cache tag', function () {
-    // config(['statamic.static_caching.strategy' => 'half']);
-
     $this->latte('{$var} {cache}{$var}{/cache}', ['var' => 'A'])
         ->assertSee('A A');
     $this->latte('{$var} {cache}{$var}{/cache}', ['var' => 'B'])
@@ -36,6 +35,18 @@ test('allow conditional caching using if param', function () {
         ->assertSee('B A');
     $this->latte('{$var} {cache if: false}{$var}{/cache}', ['var' => 'C'])
         ->assertSee('C C');
+});
+
+test('supports defining cache duration', function () {
+    $this->latte('{cache for:"1 minute"}{$var}{/cache}', ['var' => 'A'])->assertSee('A');
+    $this->latte('{cache for:"1 minute"}{$var}{/cache}', ['var' => 'B'])->assertSee('A');
+
+    Carbon::setTestNow(now());
+    $this->latte('{cache for:"2 hours"}{$var}{/cache}', ['var' => 'A'])->assertSee('A');
+    Carbon::setTestNow(now()->addHours(1));
+    $this->latte('{cache for:"2 hours"}{$var}{/cache}', ['var' => 'A'])->assertSee('A');
+    Carbon::setTestNow(now()->addHours(3));
+    $this->latte('{cache for:"2 hours"}{$var}{/cache}', ['var' => 'B'])->assertSee('B');
 });
 
 test('supports tagging cache entries', function () {
