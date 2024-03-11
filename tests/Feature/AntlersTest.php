1751a675745832165a345275409e591193b85a8f
Philipp Daun <post@philippdaun.net>
2024-03-11 15:48:38 +0100

    Expand antlers test

--- tests/Feature/AntlersTest.php
+++ tests/Feature/AntlersTest.php
@@ -1,13 +1,13 @@
 <?php
 
 test('renders inline antlers', function () {
-    $this->latte('{$title} {antlers} {{ title }} {/antlers}', ['title' => 'Zero Sugar'])
-        ->assertSee('Zero Sugar  Zero Sugar');
+    $this->latte('{$title} {antlers} {{ title }} {/antlers} {$title}', ['title' => 'Zero Sugar'])
+        ->assertSee('Zero Sugar  Zero Sugar  Zero Sugar');
 });
 
 test('does not render latte in antlers tag', function () {
-    $this->latte('{$title} {antlers} {$title} {{ title }} {/antlers}', ['title' => 'Zero Sugar'])
-        ->assertSee('Zero Sugar  {$title} Zero Sugar');
+    $this->latte('{$title} {antlers} {$title} {{ title }} {/antlers} {$title}', ['title' => 'Zero Sugar'])
+        ->assertSee('Zero Sugar  {$title} Zero Sugar  Zero Sugar');
 });
 
 test('throws when passing arguments to antlers tag', function () {
