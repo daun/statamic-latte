4151c12288444bfc4d29509e759cb3eab53bbc39
Philipp Daun <post@philippdaun.net>
2024-03-15 15:17:46 +0100

    Remove tag test

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -9,8 +9,3 @@ test('renders statamic tags directly', function () {
     $this->latte('<a href="{link to: "fanny-packs"/}"></a>')
         ->assertSee('<a href="/fanny-packs"></a>', false);
 });
-
-test('renders get_content tag', function () {
-    $this->latte('{get_content from:"/testable"}Things{/get_content}')
-        ->assertSee('Things', false);
-});
