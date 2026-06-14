865a613ea636b2689e7d4cdb51503239ca4cc761
Philipp Daun <post@philippdaun.net>
2026-06-14 17:34:49 +0200

    Test capturing paginator into a variable

--- tests/Feature/HelperTest.php
+++ tests/Feature/HelperTest.php
@@ -14,6 +14,19 @@ describe('helpers', function () {
             ->assertDontSee('Testable,');
     });
 
+    test('captures a paginator into a variable for foreach and page meta', function () {
+        $this->latte(<<<'LATTE'
+            {var $entries = s("collection", ["from" => "pages", "order" => "title", "paginate" => 1])}
+            {foreach $entries as $entry}
+                |{$entry->title}|
+            {/foreach}
+            Showing page {$entries->currentPage()} of {$entries->lastPage()}, {$entries->total()} total
+        LATTE)
+            ->assertSee('|Testable|')
+            ->assertDontSee('Testable With Layout')
+            ->assertSee('Showing page 1 of 2, 2 total');
+    });
+
     test('provides the s() helper function as an alias', function () {
         $this->latte('<a href="{s(link, to: "fanny-packs")}"></a>')
             ->assertSee('<a href="/fanny-packs"></a>', false);
